a87105bd94d298305e1bae485fb615245f00cdee
rohan9024 <rohanwandre24@gmail.com>
2024-04-30 22:58:11 +0530

    Final changes

--- components/Navbar.js
+++ components/Navbar.js
@@ -15,11 +15,11 @@ function Navbar() {
             <div class="w-screen py-6 px-10 flex justify-between items-center">
                 <Link href="/" class={`${poppins.className} text-lg font-medium cursor-pointer`}>SIES Graduate School of Technology</Link>
                 <div class="flex justify-center items-center space-x-10">
-                    <Link href="/admin-panel" class={`${poppins.className} text-sm font-medium cursor-pointer hover:ease-in transition  hover:text-gray-400`}>Inventory</Link>
-                    <Link href="/auditorium-booking" class={`${poppins.className} text-sm font-medium cursor-pointer hover:ease-in transition  hover:text-gray-400`}>Auditorium Booking</Link>
-                    <Link href="/view-requests" class={`${poppins.className} text-sm font-medium cursor-pointer hover:ease-in transition  hover:text-gray-400`}>View Requests</Link>
-                    <Link href="/departments" class={`${poppins.className} text-sm font-medium cursor-pointer hover:ease-in transition  hover:text-gray-400`}>Departments</Link>
-                    <Link href="/profile" class={`${poppins.className} text-sm font-medium cursor-pointer hover:ease-in transition  hover:text-gray-400`}>Profile</Link>
+                    <Link href="/admin-panel" class={`${poppins.className} text-sm font-medium cursor-pointer  hover:text-gray-400`}>Inventory</Link>
+                    <Link href="/auditorium-booking" class={`${poppins.className} text-sm font-medium cursor-pointer   hover:text-gray-400`}>Auditorium Booking</Link>
+                    <Link href="/view-requests" class={`${poppins.className} text-sm font-medium cursor-pointer   hover:text-gray-400`}>View Requests</Link>
+                    <Link href="/departments" class={`${poppins.className} text-sm font-medium cursor-pointer  hover:text-gray-400`}>Departments</Link>
+                    <Link href="/profile" class={`${poppins.className} text-sm font-medium cursor-pointer  hover:text-gray-400`}>Profile</Link>
                 </div>
             </div>
             <div className='bg-gray-300 h-[1px] ' />
--- src/app/profile/page.js
+++ src/app/profile/page.js
@@ -7,7 +7,8 @@ import Navbar from '../../../components/Navbar';
 import { Inter, Raleway } from 'next/font/google';
 import { useRouter } from 'next/navigation';
 import { AuthContext } from "../../../contexts/AuthContext"
-import { updateDoc } from 'firebase/firestore';
+import { doc, updateDoc } from 'firebase/firestore';
+import { db } from '../../../firebase';
 
 
 const raleway = Raleway({
@@ -124,7 +125,7 @@ function page() {
                         <input onChange={(e) => setPassword(e.target.value)} required type="password" placeholder="Password" className={`${inter.className} placeholder:text-gray-800 px-5 py-2  outline-none border border-gray-800 w-96`} />
 
 
-                        <div disabled={!username || !password} type="submit" class="cursor-pointer relative inline-flex items-center px-12 py-3 overflow-hidden text-lg font-medium text-black border-2 border-black rounded-full hover:text-white group hover:bg-gray-50 w-96 mx-auto">
+                        <div onClick={updateAdmin} disabled={!username || !password} type="submit" class="cursor-pointer relative inline-flex items-center px-12 py-3 overflow-hidden text-lg font-medium text-black border-2 border-black rounded-full hover:text-white group hover:bg-gray-50 w-96 mx-auto">
                             <span class="absolute left-0 block w-full h-0 transition-all bg-black opacity-100 group-hover:h-full top-1/2 group-hover:top-0 duration-400 ease"></span>
                             <span class="absolute right-0 flex items-center justify-start w-10 h-10 duration-300 transform translate-x-full group-hover:translate-x-0 ease">
                                 <svg class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M14 5l7 7m0 0l-7 7m7-7H3"></path></svg>
